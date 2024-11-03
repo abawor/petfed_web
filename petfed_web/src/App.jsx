857585d73e7c3c26c2dedc2ee5b17b5b6cfedcb7
axiu <abawor200@gmail.com>
2024-11-03 08:30:45 +0000

    Moved nav to bottom of screen and updated icons

--- petfed_web/src/App.jsx
+++ petfed_web/src/App.jsx
@@ -1,25 +1,47 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen';
 import AddNewPet from './pages/AddNewPet';
+import { IoHomeOutline } from "react-icons/io5";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { MdSchedule } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
 import './App.css'
 
 
 function App() {
   return (
     <Router>
-      {/* Navigation Bar */}
-      <nav className="flex justify-around p-4 bg-gray-100 border-b border-gray-300">
-        <Link to="/" className="text-lg text-black">Home</Link>
-        <Link to="/" className="text-lg text-black">Meals</Link>
-        <Link to="/" className="text-lg text-black">Schedule</Link>
-        <Link to="/" className="text-lg text-black">Settings</Link>
-      </nav>
+      <div className="flex flex-col h-screen justify-between">
+    
+        {/* Routes for the app */}
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/add-new-pet" element={<AddNewPet />} />
+        </Routes>
 
-      {/* Routes for the app */}
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/add-new-pet" element={<AddNewPet />} />
-      </Routes>
+
+
+        {/* Navigation Bar */}
+        <nav className="flex justify-around p-4 bg-gray-100">
+          <Link to="/" className="text-lg">
+            <IoHomeOutline size={55} className=" mx-auto"/>
+            Home
+          </Link>
+          <Link to="/" className="text-lg">
+            <IoFastFoodOutline size={55} className=" mx-auto"/>
+            Meals
+          </Link>
+          <Link to="/" className="text-lg">
+            <MdSchedule size={55} className=" mx-auto"/>
+            Schedule
+          </Link>
+          <Link to="/" className="text-lg">
+            <IoSettingsOutline size={55} className=" mx-auto"/>
+            Settings
+          </Link>
+        </nav>
+
+      </div>
     </Router>
   );
 }
