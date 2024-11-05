1527a99387a7a333ed1bd286386c8c4489862c20
axiu <abawor200@gmail.com>
2024-11-05 06:54:39 +0000

    Added safeareaview

--- petfed_web/src/App.jsx
+++ petfed_web/src/App.jsx
@@ -3,6 +3,7 @@ import HomeScreen from './pages/HomeScreen';
 import MealsScreen from './pages/MealsScreen';
 import AddNewMeal from './pages/AddNewMeal';
 import AddNewPet from './pages/AddNewPet';
+import { SafeAreaView } from 'react-native-web';
 import { PetProvider } from './components/PetContext';
 import { MealsProvider } from './components/MealsContext';
 import { IoHomeOutline } from "react-icons/io5";
@@ -14,42 +15,44 @@ import './App.css'
 
 function App() {
   return (
-    <PetProvider>
-      <MealsProvider>
-        <Router>
-          <div className="flex flex-col h-screen justify-between">
-        
-            {/* Routes for the app */}
-            <Routes>
-              <Route path="/" element={<HomeScreen />} />
-              <Route path="/add-new-pet" element={<AddNewPet />} />
-              <Route path="/meals" element={<MealsScreen />} />
-              <Route path="/add-new-meal" element={<AddNewMeal />} />
-            </Routes>
+    <SafeAreaView className="flex">
+      <PetProvider>
+        <MealsProvider>
+          <Router>
+            <div className="flex flex-col h-screen justify-between">
+          
+              {/* Routes for the app */}
+              <Routes>
+                <Route path="/" element={<HomeScreen />} />
+                <Route path="/add-new-pet" element={<AddNewPet />} />
+                <Route path="/meals" element={<MealsScreen />} />
+                <Route path="/add-new-meal" element={<AddNewMeal />} />
+              </Routes>
 
-            {/* Navigation Bar */}
-            <nav className="flex justify-around p-4 bg-teal-400">
-              <Link to="/" className="text-lg font-bold text-white">
-                <IoHomeOutline size={55} className="mx-auto stroke-0"/>
-                Home
-              </Link>
-              <Link to="/meals" className="text-lg font-bold text-white ">
-                <IoFastFoodOutline size={55} className="mx-auto"/>
-                Meals
-              </Link>
-              <Link to="/" className="text-lg font-bold text-white">
-                <MdSchedule size={55} className="mx-auto"/>
-                Schedule
-              </Link>
-              <Link to="/" className="text-lg font-bold text-white">
-                <IoSettingsOutline size={55} className="mx-auto"/>
-                Settings
-              </Link>
-            </nav>
-          </div>
-        </Router>
-      </MealsProvider>
-    </PetProvider>
+              {/* Navigation Bar */}
+              <nav className="flex justify-around p-4 bg-teal-400">
+                <Link to="/" className="text-lg font-bold text-white">
+                  <IoHomeOutline size={55} className="mx-auto stroke-0"/>
+                  Home
+                </Link>
+                <Link to="/meals" className="text-lg font-bold text-white ">
+                  <IoFastFoodOutline size={55} className="mx-auto"/>
+                  Meals
+                </Link>
+                <Link to="/" className="text-lg font-bold text-white">
+                  <MdSchedule size={55} className="mx-auto"/>
+                  Schedule
+                </Link>
+                <Link to="/" className="text-lg font-bold text-white">
+                  <IoSettingsOutline size={55} className="mx-auto"/>
+                  Settings
+                </Link>
+              </nav>
+            </div>
+          </Router>
+        </MealsProvider>
+      </PetProvider>
+    </SafeAreaView>
   );
 }
 
