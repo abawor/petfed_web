c60683bdc745b74bc2719cf54c36f7512002199f
tensARC-ABawor <axel@shadeplus.co.uk>
2024-11-07 13:35:19 +0000

    Removed incompatible day and time selectors and added new day selector

--- petfed_web/src/components/PetList.jsx
+++ petfed_web/src/components/PetList.jsx
@@ -8,18 +8,20 @@ export default function PetList() {
 
     return (
         <ul className="grid grid-cols-3 gap-4">
-            <li>
-                <Link
-                    to="/add-new-pet"
-                    className="flex justify-center items-center h-32 w-32 py-3 bg-teal-100 text-teal-500 rounded-full border-solid border-4 border-teal-500"
-                >
-                    <FaPlus size={75}/>
-                </Link>
-            </li>
+            <div key={ 1 }>
+                <li>
+                    <Link
+                        to="/add-new-pet"
+                        className="flex justify-center items-center h-32 w-32 py-3 bg-teal-100 text-teal-500 rounded-full border-solid border-4 border-teal-500"
+                    >
+                        <FaPlus size={75}/>
+                    </Link>
+                </li>
+            </div>
             {pets.map((item) => {
                 return (
-                    <div>
-                        <li className="flex w-32 h-32 overflow-hidden rounded-full" key={ item.id }>
+                    <div key={ item.id }>
+                        <li className="flex w-32 h-32 overflow-hidden rounded-full">
                             <img src={ item.photo } className="object-cover h-full w-full"/>
                         </li>
                         <p>{item.name}</p>
--- petfed_web/src/pages/AddNewSchedule.jsx
+++ petfed_web/src/pages/AddNewSchedule.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { ScheduleContext } from '../components/ScheduleContext';
 import { useNavigate } from 'react-router-dom';
-import { DropdownSelector } from 'reactjs-weekdays-picker';
-import TimePicker from 'rc-time-picker';
-import 'rc-time-picker/assets/index.css';
+import Select from 'react-select';
 
 export default function AddNewMeal() {
     const { setSchedule } = useContext(ScheduleContext);
@@ -12,6 +10,18 @@ export default function AddNewMeal() {
     const [time, setTime] = useState('');
     const navigate = useNavigate();
 
+    const weekdays = [
+        { value: 'monday', label: 'Monday' },
+        { value: 'Tuesday', label: 'Tuesday' },
+        { value: 'Wednesday', label: 'Wednesday' },
+        { value: 'Thursday', label: 'Thursday' },
+        { value: 'Friday', label: 'Friday' },
+        { value: 'Saturday', label: 'Saturday' },
+        { value: 'Sunday', label: 'Sunday' },
+    ]
+
+
+    console.log(days)
     const handleSave = () => {
         if (!name || !days || !time ) {
             alert('All fields are required!');
@@ -45,31 +55,16 @@ export default function AddNewMeal() {
                     className="mb-4 p-2 w-full border border-slate-500 rounded-md"
                 />
 
-                <div className="mb-4 leading-7 w-full border border-slate-500 rounded-md">
-                    <DropdownSelector
-                        state={days}
-                        setState={setDays}
-                        dayList={['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']}
-                        multiple={true}
-                        selectedColor="#14b8a6"
-                        unselectedColor="#ffffff"
-                        selectedHoverColor="#14b8a6"
-                        unselectedHoverColor="#ffffff"
-                        width="100%"
-                        fontSize="30"
-                        placeholder=" Days"
+                <div className="mb-4 leading-8 text-left w-full border border-slate-500 rounded-md">
+                    <Select
+                        options={weekdays}
+                        isMulti={true}
+                        onChange={setDays}
+                        placeholder="Days"
                     />
                 </div>
 
-                <div className="mb-4 w-full border border-slate-500 rounded-md">
-                    <TimePicker
-                        className="w-full"
-                        popupClassName="custom-time-picker-popup"
-                        showSecond={false}
-                        format={"h:mm a"}
-                        placeholder="Time"
-                    />
-                </div>
+
 
                 <button
                 type="button"
