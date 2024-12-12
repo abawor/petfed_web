ef741f9d7d9792741d298e043215df5a985340e8
abawor <abawor200@gmail.com>
2024-12-12 11:49:19 +0000

    Added local save for Global Notifications setting

--- petfed_web/src/pages/SettingsScreen.jsx
+++ petfed_web/src/pages/SettingsScreen.jsx
@@ -1,10 +1,15 @@
-import React, { useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Toggle from 'react-toggle'
 import 'react-toggle/style.css';
 
 
 export default function SettingsScreen() {
-    const [notifications, setNotificiations] = useState(true)
+    const savedNotifications = localStorage.getItem("globalNotifications")
+    const [notifications, setNotificiations] = useState(savedNotifications ? savedNotifications === "true" : true)
+
+    useEffect(() => {
+        localStorage.setItem("globalNotifications", notifications)
+    }, [notifications])    
 
     return (
         <div className='text-2xl flex items-center m-10'>
